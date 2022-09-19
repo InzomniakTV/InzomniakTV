c972255881a94413ce59218bbb7faf7337fed8d7
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-09-19 19:42:52 -0400

    Update NumberGuesser.py

--- school/PythonClass/Projects/NumberGuesser.py
+++ school/PythonClass/Projects/NumberGuesser.py
@@ -2,16 +2,12 @@
 
 from random import *
 x = (randint(1, 10))
-
 print("Let's play a game!")
-
+print('------------------')
 y = int(input("Pick a number 1-10: "))
-if (y >= 1, y <= 10):
+
+if 1 <= y <= 10:
     print("So, you've chosen", int(y), 'it seems.')
 else:
     y = int(input('Try again I said 1-10, moron...'))
 
-if (y == x):
-    print('Wow you guessed the right number it really was', int(x), 'holy crap!')
-else:
-    print('HAHA Idiot, it was', int(x), 'you dingleberry!  TRY AGAIN!')
