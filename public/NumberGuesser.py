b1122cc09684e579974db1f59feb7d7be5e79ded
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-09-21 00:22:13 -0400

    v0.2 (basically a copy paste just to move, no edits yet)

---
+++ public/NumberGuesser.py
@@ -0,0 +1,12 @@
+# Number guesser v0.2
+
+from random import *
+x = (randint(1, 10))
+print("Let's play a game!")
+print('------------------')
+y = int(input("Pick a number 1-10: "))
+
+if 1 <= y <= 10:
+    print("So, you've chosen", int(y), 'it seems.')
+else:
+    y = int(input('Try again I said 1-10, moron...'))
