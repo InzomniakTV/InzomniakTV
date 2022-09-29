0978b6c9d240160265fc5b2662ecad216bb6c087
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-09-28 22:53:15 -0400

    Danny my boy is cute

--- public/OSRS-Gambler/rollthedice.py
+++ public/OSRS-Gambler/rollthedice.py
@@ -5,11 +5,13 @@
 
 # Project Started 9/28/2022
 
-cash = 300
+cash = '{:,}'.format(300000000)
 
 # I want this to do something where money auto sets it's denomination
 # when it goes over/under certain thresholds and for it to show it's
-# denomination whenever it prints the value "cash".
-def main(money):
-    
+# denomination whenever it prints the value "cash".  For now it's commented
+# because if you run the script it fails, but this is a placeholder for a
+# planned feature.  Will revisit later.
+# def main(money):
+
 print(cash)
