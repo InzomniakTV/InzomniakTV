05506eae3b8de14a097ab8dabeb5e8f94014775e
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-09-28 22:38:13 -0400

    Uhh

---
+++ public/OSRS-Gambler/rollthedice.py
@@ -0,0 +1,10 @@
+# Inzomniak and Injured Sock Present -
+#########################################
+####### OSRS Python Gambler v1.0 ########
+#########################################
+
+# Project Started 9/28/2022
+
+cash = 300
+
+print(cash)
---
+++ public/OSRS-Gambler/run.bat
@@ -0,0 +1,5 @@
+ECHO ON
+REM OSRS Gambler
+C:\Users\inzom\anaconda3\python.exe
+D:\GitHub\public\InzomniakTV\public\OSRS-Gambler\rollthedice.py
+PAUSE
