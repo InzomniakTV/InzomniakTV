b7252e8929d0cb190b9cf47001be186286f76223
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-09-28 23:44:03 -0400

    Couldn't resit.

--- public/OSRS-Gambler/rollthedice.py
+++ public/OSRS-Gambler/rollthedice.py
@@ -5,7 +5,22 @@
 
 # Project Started 9/28/2022
 
-cash = '{:,}'.format(300000000)
+from random import *
+cash = 300000000
+
+print("Alright numbnuts, listen up.")
+print("You're gonna waste your life chasing GP.")
+bet = int(input("Enter your first bet, it's a 50/50 chance: "))
+
+# Simple 50/50 flipper.
+def stake():
+    fight = (randint(0, 1))
+    if fight == 1:
+        print("{:,}".format(cash + bet))
+    else:
+        print("{:,}".format(cash - bet))
+
+stake()
 
 # I want this to do something where money auto sets it's denomination
 # when it goes over/under certain thresholds and for it to show it's
@@ -13,5 +28,3 @@ cash = '{:,}'.format(300000000)
 # because if you run the script it fails, but this is a placeholder for a
 # planned feature.  Will revisit later.
 # def main(money):
-
-print(cash)
