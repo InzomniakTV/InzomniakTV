c3badc415288d871f845dfa48587018cf07d3a7f
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-10-10 18:21:31 -0400

    new smol kine lab for python

---
+++ PythonClass/Projects/Lab07/age_average.py
@@ -0,0 +1,15 @@
+# ITP-100 Software Design
+# Student: Jeannotte, Michael
+# Instructor: Brown, Georgia
+# Date given to class: 9-12-2022
+# Date of Submission:
+# Description: Conversions of KPH to MPH in increments of 10
+# Input: Employee gross/bonus pay.
+# Output: Retirement contribution
+# Additional Comments: V 1.0
+
+me = int(input('Please enter your age.''))
+cindy = int(input('Please enter the age of Cindy.'))
+josh = int(input('Please enter the age of Josh.'))
+avg = float(josh+cindy+me)/3
+print(avg)
