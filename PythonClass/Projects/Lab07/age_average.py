9598d1e42a4d56165dbe62835b27b9b66ffa5073
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-10-10 18:22:27 -0400

    Header update.

--- PythonClass/Projects/Lab07/age_average.py
+++ PythonClass/Projects/Lab07/age_average.py
@@ -1,14 +1,14 @@
 # ITP-100 Software Design
 # Student: Jeannotte, Michael
 # Instructor: Brown, Georgia
-# Date given to class: 9-12-2022
-# Date of Submission:
-# Description: Conversions of KPH to MPH in increments of 10
-# Input: Employee gross/bonus pay.
-# Output: Retirement contribution
+# Date given to class: 10/10/2022
+# Date of Submission: 10/10/2022
+# Description: Average out ages.
+# Input: 3 persons' age.
+# Output: Average age of the 3 people.
 # Additional Comments: V 1.0
 
-me = int(input('Please enter your age.''))
+me = int(input('Please enter your age.'))
 cindy = int(input('Please enter the age of Cindy.'))
 josh = int(input('Please enter the age of Josh.'))
 avg = float(josh+cindy+me)/3
