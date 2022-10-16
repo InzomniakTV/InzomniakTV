651b388150004c1b68b939b8bc6233077c1a5d97
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-10-15 23:00:33 -0400

    Changed more stuff like delting copy pastas.

--- PythonClass/Projects/Lab08/SoftwareSales.py
+++ PythonClass/Projects/Lab08/SoftwareSales.py
@@ -7,21 +7,8 @@
 # Input: 3 persons' age.
 # Output: Average age of the 3 people.
 # Additional Comments: V 1.0
-# 12. Software Sales
-# A software company sells a package that retails for $99.
-# Quantity discounts are given according to the following
-# table:
-#
-# Quantity      Discount
-# 10–19         10%
-# 20–49         20%
-# 50–99         30%
-# 100 or more   40%
-#
-# Write a program that asks the user to enter the number of
-# packages purchased. The program should then display the
-# amount of the discount (if any) and the total amount of
-# the purchase after the discount.
+
+
 
 PRICE_PER_PACKAGE = 99.00
 
