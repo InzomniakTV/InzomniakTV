f77a704409fb6519f92fcdd5d5024cc4bc8578bc
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-10-17 18:32:54 -0400

    Yes

--- public/NumberGuesser.py
+++ public/NumberGuesser.py
@@ -1,13 +1,1 @@
 # Inzomniak Public Code
-# Number Guessser v0.3
-
-from random import *
-x = (randint(1, 10))
-print("Let's play a game!")
-print('------------------')
-y = int(input("Pick a number 1-10: "))
-
-if 1 <= y <= 10:
-    print("So, you've chosen", int(y), 'it seems.')
-else:
-    y = int(input('Try again I said 1-10, moron...'))
