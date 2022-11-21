930b8fb08ce0f9717cbcd73faa6b89272fdf45a6
InzomniakTV <111333499+InzomniakTV@users.noreply.github.com>
2022-11-20 22:52:41 -0500

    New code stuff for lists idk

--- public/randomcode.py
+++ public/randomcode.py
@@ -1,2 +1,22 @@
-values = [2] * 5
-print(values)
+# 1. PYTHON LISTS
+
+# Explanation: Creating a list...
+print("\nCreating a list...")
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+
+# Explanation:
+print("\nCreating another list, same elements, different name...")
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# Explanation:
+print("\nLength of a list")
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# 2. ACCESS
+
+# 3. CHANGE
+
+# 4. ADD
